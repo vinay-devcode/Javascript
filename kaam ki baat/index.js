ed2236eb365d4f13d299caab4c71d597c0e69acf
codecrafter <vinaysaini56819@gmail.com>
2026-02-17 21:41:09 +0530

    interview main kaam ayaga

---
+++ kaam ki baat/index.js
@@ -0,0 +1,8 @@
+console.log( typeof null); //  type of null is object   
+console.log( typeof undefined); //  type of undefined is undefined
+console.log( typeof NaN); //  type of NaN is number
+console.log( typeof []); //  type of array is object
+console.log( typeof {}); //  type of object is object
+console.log( typeof function(){}); //  type of function is function
+
+
