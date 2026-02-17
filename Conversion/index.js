7b56b7cea2bedca97a7293ca0df06464c549758b
codecrafter <vinaysaini56819@gmail.com>
2026-02-17 22:02:23 +0530

    conversion

---
+++ Conversion/index.js
@@ -0,0 +1,24 @@
+let score = 33;
+
+console.log(typeof score); // type of score is number
+
+score = "33";   
+console.log(typeof score); // type of score is string
+console.log(score); // value of score is 33
+
+score = true;   
+console.log(typeof score); /// type of score is boolean
+
+
+// 1=> and convert it into boolean give s true
+// 0=> and convert it into boolean give s false
+// ""=> and convert it into boolean give s false    
+// " "=> and convert it into boolean give s true
+// null=> and convert it into boolean give s false
+// undefined=> and convert it into boolean give s false
+// NaN=> and convert it into boolean give s false
+// []=> and convert it into boolean give s true
+// {}=> and convert it into boolean give s true 
+// "hitsh" => and convert it into boolean give s true
+// 
+
